ead89db3f987b5c80b3d48f6ee48ef440dff5d72
Sebastian Świczerewski <stigunknown21@gmail.com>
2025-03-01 20:30:58 +0100

    Multiprocessing update

---
+++ mp_demo.py
@@ -0,0 +1,17 @@
+import time
+import multiprocessing
+
+
+def mySleep(n):
+    print(f'Sleeping for {n}')
+    time.sleep(n)
+
+def main():
+    l = [ 1, 2, 3, 2, 4, 2, 3, 5, 3, 2]
+
+    pool = multiprocessing.Pool(4)
+    pool.map(mySleep, l)
+
+
+if __name__ == '__main__':
+    main()
